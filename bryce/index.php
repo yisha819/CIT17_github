d269d86669c012af705a3de6b38e7cc6e405724a
M307user <M307user@lab.uc-bcf.local>
2025-09-27 08:19:30 +0800

    Added php file

---
+++ bryce/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php demo</title>
+</head>
+<body>
+    <h1>Php Demo</h1>
+
+    <?php
+        echo "BIODATA";
+        echo "<ul>";
+        echo "<li><b>Name:</b> Ayisha Hailey L. Baterina </li>";
+        echo "<br>";
+        echo "<li> <b>Present Address:</b> MB-194, Pul-og, Puguis, La Trinidad, Benguet </li>";
+        echo "<br>";
+        echo "<li><b>Birthdate:</b> November 23,2004</li>";
+        echo "<br>";
+        echo "<li><b>Contact No.:</b> +639284014175</li>";
+        echo "<br>";
+        echo "<li><b>Email:</b> haileybaterina@gmail.com </li>";
+        echo "<br>";
+        echo "<li><b>Age:</b> 20</li>";
+        echo "<br>";
+        echo "<li><b>Marital Status:</b> Single </li>";
+        echo "<br>";
+        echo "<li><b>Occupation:</b> Student</li>";
+        echo "<br>";
+        echo "<li><b>Educational Background:</b> <p>Bachelor of Science in Information Technology </p> University of the Cordilleras </li>";
+
+    ?>
+    
+
+</body>
+</html>
